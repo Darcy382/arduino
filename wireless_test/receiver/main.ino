36394b0f31a2954144d93046fa60b0aac143554c
Kyle D'Arcy <kyle.darcy@icloud.com>
2021-12-18 16:12:28 -0500

    second test, nothing

--- wireless_test/receiver/main.ino
+++ wireless_test/receiver/main.ino
@@ -1,42 +1,31 @@
-//nrf2401 receiver
+/*
+* Arduino Wireless Communication Tutorial
+* Example 1 - Receiver Code
+*
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+*
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 nrf(9, 8);  // CE, CSN
-const byte linkAddress[6] = "00001";  //address through which two modules communicate.
-const byte greenLED = 2;
-float data[3];
-/////////////////////////////////////////////////////////////////////
-void setup()
-{
-  Serial.begin(9600); 
-  Serial.println("Starting");
-  pinMode(greenLED, OUTPUT);
- 
-  nrf.begin();    
-  nrf.openReadingPipe(0, linkAddress);  //set the address 
-  nrf.startListening();   //Set nrf as receiver
+RF24 radio(7, 8); // CE, CSN
+
+const byte address[6] = "00001";
+
+void setup() {
+    Serial.begin(9600);
+    radio.begin();
+    radio.openReadingPipe(0, address);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.startListening();
 }
-///////////////////////////////////////////////////
-void loop()
-{
-//    digitalWrite(greenLED, HIGH);
-   delay(50);
-//    digitalWrite(greenLED, LOW);
-   delay(50);
-   
-   if (nrf.available())  //Read the data if available in buffer
-     {
-    digitalWrite(greenLED, LOW);
-    // digitalWrite(greenLED, HIGH);
-      nrf.read(&data, sizeof(data));
-     Serial.print(data[0]); Serial.print("hPa  ");
-     Serial.print(data[1]); 
-     Serial.write(0xC2);  //send degree symbol
-     Serial.write(0xB0);  //send degree symbol
-     Serial.print("C   ");  
-     Serial.print(data[2]); Serial.println("m");  
-     }  
+
+void loop() {
+    if (radio.available()) {
+        char text[32] = "";
+        radio.read(&text, sizeof(text));
+        Serial.println(text);
+    }
 }
-//////////////////////////////////////////////
--- wireless_test/transmitter/main.ino
+++ wireless_test/transmitter/main.ino
@@ -1,66 +1,27 @@
-//nrf2401 transmitter:
-// #include "DHT.h"
+/*
+* Arduino Wireless Communication Tutorial
+* Example 1 - Transmitter Code
+*
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+*
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-// #define DHTPIN 2
-// #define DHTTYPE DHT11
-RF24 nrf(9, 8);  // CE, CSN
 
-// DHT dht(DHTPIN, DHTTYPE);
+RF24 radio(7, 8); // CE, CSN
 
-const byte linkAddress[6] = "00001";
+const byte address[6] = "00001";
 
-float humidity;
-float temperature;  
-float heatIndex; 
-float data[3];
-char charVal[17];
-////////////////////////////////////////////////////
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println("NRF24L01 link");
-  nrf.begin();   
-//   dht.begin();
-  nrf.openWritingPipe(linkAddress);  //set the address 
-  //nrf.setPALevel(RF24_PA_LOW);   //keep tx level low to stop psu noise, can remove this but then put in decoupling caps
-  // Options are: RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm,  RF24_PA_HIGH=0dBm.
-  nrf.setPALevel(RF24_PA_MIN); 
-  nrf.stopListening();  //act as a transmitter
+void setup() {
+    radio.begin();
+    radio.openWritingPipe(address);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.stopListening();
 }
-///////////////////////////////////////////////////
-void loop()
-{  
-//  humidity = dht.readHumidity();
-//  temperature = dht.readTemperature(true);
-//  heatIndex = dht.computeHeatIndex(temperature, humidity);
-
- humidity = 10.0;
- temperature = 11.0;
- heatIndex = 12.0;
-  
- data[0] = humidity;
- data[1] = temperature;
- data[2] = heatIndex;
-//  ----- display on local monitor: ------------
- Serial.print("Humidity: ");
- Serial.print(data[0]);
- Serial.print("% ");
- Serial.print("Temp: ");
- Serial.print(data[1]); 
- Serial.write(0xC2);  //send degree symbol
- Serial.write(0xB0);  //send degree symbol
- Serial.print("F   ");  
- Serial.print("Heat Index: ");
- Serial.print(data[2]);
- Serial.write(0xC2);  //send degree symbol
- Serial.write(0xB0);  //send degree symbol
- Serial.println("F   ");  
-//  ---------------------------------------------
- 
-  nrf.write(data, sizeof(data)); //spit out the data array
-  
-  delay(300);
+void loop() {
+    const char text[] = "Hello World";
+    radio.write(&text, sizeof(text));
+    delay(1000);
 }
-/////////////////////////////////////////////////
